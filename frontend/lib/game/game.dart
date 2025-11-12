31b4543c331a3c9372843868dfca5c8b7a26ee37
TheAngryPopo <joelharder4@gmail.com>
2025-11-12 16:22:51 -0500

    change sample game to be the chef in the kitchen

--- frontend/lib/game/game.dart
+++ frontend/lib/game/game.dart
@@ -2,19 +2,21 @@ import 'package:chaos_kitchen/game/cook_world.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
 
 class ChaosKitchenGame extends FlameGame {
   ChaosKitchenGame() : super(world: CookWorld());
 
   @override
-  Color backgroundColor() {
-    return const Color.fromARGB(255, 173, 223, 247);
-  }
-
-  @override
   Future<void> onLoad() async {
     camera.viewfinder.anchor = Anchor.topLeft;
+
+    const scaleFactor = 1.5; // larger value = more zoomed in
+    final background = SpriteComponent()
+      ..sprite = await loadSprite('backgrounds/kitchen.png')
+      ..size = size * scaleFactor
+      ..anchor = Anchor.topLeft
+      ..position = -((size * scaleFactor - size) / 2);
+    add(background);
   }
 
   @override
